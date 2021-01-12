185a69cc17e2719b0f9fd790531929a58643e93f
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-01-12 11:03:37 -0500

    Update with another HTTPS attempt

--- __zchsh/connect.sh
+++ __zchsh/connect.sh
@@ -1,5 +1,6 @@
 # Run `chmod u+x connect.sh` in the directory that contains this file.
 # Place tf-sourcegraph-test.pem adjacent to this file.
+# Run `terraform apply` and update the address after ec2-user@
 # Then run `./connect.sh`
 chmod 400 tf-sourcegraph-test.pem
-ssh -i "tf-sourcegraph-test.pem" ec2-user@ec2-3-139-6-228.us-east-2.compute.amazonaws.com
+ssh -i "tf-sourcegraph-test.pem" ec2-user@ec2-3-140-12-172.us-east-2.compute.amazonaws.com
